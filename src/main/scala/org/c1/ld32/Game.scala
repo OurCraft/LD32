0ca6543f45b3893fa397ebcc4b88a8e1b5d6cb7e
Thog <thog92@hotmail.fr>
2015-04-18 05:07:27 +0200

    Add Game Class

---
+++ src/main/scala/org/c1/ld32/Game.scala
@@ -0,0 +1,58 @@
+package org.c1.ld32
+
+import org.lengine.GameBase
+import org.lengine.level.Level
+import org.lengine.render.{FontRenderer, TextureAtlas}
+import org.lengine.tests.EntityPlayer
+
+object Game extends GameBase("LD32") {
+
+  var level: Level = _
+
+  var fontRenderer: FontRenderer = _
+
+  var player: EntityPlayer = _
+
+  override def getBaseHeight: Int = 640
+
+  override def update(delta: Float): Unit = {
+    level.update(delta)
+  }
+
+  override def initGame: Unit = {
+    fontRenderer = new FontRenderer(new TextureAtlas("assets/textures/font.png", 16, 16))
+
+    level = new Level
+    player = new EntityPlayer
+    level spawn player
+  }
+
+  override def onKeyReleased(keyCode: Int, char: Char): Unit = {
+
+  }
+
+  override def onMouseMoved(x: Int, y: Int, dx: Int, dy: Int): Unit = {
+
+  }
+
+  override def onKeyPressed(keyCode: Int, char: Char): Unit = {
+
+  }
+
+  override def render(delta: Float): Unit = {
+    level.render(delta)
+    fontRenderer.renderString("LD32: An Unconventional Weapon", 0, 0, 0xFF00FFFF, 2)
+  }
+
+  override def onMousePressed(x: Int, y: Int, button: Int): Unit = {
+
+  }
+
+  override def onMouseReleased(x: Int, y: Int, button: Int): Unit = {
+
+  }
+
+  override def onScroll(x: Int, y: Int, dir: Int): Unit = {
+
+  }
+}
